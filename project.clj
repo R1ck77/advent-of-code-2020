563b47141ff8af8f916e37b23c4f285d584a1e5c
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-12-05 12:31:54 +0100

    Add information to project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject advent-of-code-2020 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "My solutions for the \"Advent of code 2020\" (https://adventofcode.com/)"
+  :license {:name "WTFNMFPL-1.0 (Do What The F*** You Want To But It's Not My Fault Pulic License v1.0"
+            :url "https://tldrlegal.com/license/do-what-the-fuck-you-want-to-but-it's-not-my-fault-public-license-v1-(wtfnmfpl-1.0)"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :repl-options {:init-ns advent-of-code-2020.core}
   :main advent-of-code-2020.core/advent-of-code-2020)
